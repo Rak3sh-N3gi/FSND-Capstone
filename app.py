ef809bdaade86f1448947f8fc7778dbdce22e4f8
Rakesh Negi <rak3shn3gi@gmail.com>
2025-03-08 10:17:13 +0530

    initial working code

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from flask_cors import CORS
 def create_app(test_config=None):
 
     app = Flask(__name__)
-    setup_db(app)
-    CORS(app)
+    with app.app_context():
+        setup_db(app)
+        CORS(app)
 
     @app.route('/')
     def get_greeting():
