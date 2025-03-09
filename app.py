168e3da113fa167600f988f48e83af1b87762d35
Rakesh Negi <rak3shn3gi@gmail.com>
2025-03-09 23:50:24 +0530

    All functioanlities coded

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask
-from models import setup_db
+from flask import Flask, request, jsonify
+from sqlalchemy import null
+from models import *
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
+    # db.init_app(app)
+    migrate = Migrate(app, db)
     with app.app_context():
         setup_db(app)
         CORS(app)
@@ -22,37 +27,120 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
     
+    # Get actors by actor ID
+    @app.route('/actors/<int:id>')
+    def get_actor(id):
+        actors = Actors.display(id)
+        return json.loads(actors)
+    
+    # Get movies by movie ID
+    @app.route('/movies/<int:id>')
+    def get_movie(id):
+        movies = Movies.display(id)
+        return json.loads(movies)
+
+    # Get all actors
     @app.route('/actors')
     def get_actors():
-        return "Get actors"
-    
+        actors = Actors.display_all()
+        return json.loads(actors)
+
+    # Get all movies
     @app.route('/movies')
     def get_movies():
-        return "Get movies"
+        movies = Movies.display_all()
+        return json.loads(movies)
     
-    @app.route('/actors', methood=['DELETE'])
-    def delete_actor():
-        return "Delete actor"
+    # Delete actor by actor ID
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    def delete_actor(id):
+        rc = Actors.delete(id)
+        if rc == 0:
+            return "Actor deletion failed."
+        else:
+            return ("Actor with id=%d deleted successfully" % id)
     
-    @app.route('/movies', methood=['DELETE'])
-    def delete_movie():
-        return "Delete movie"
+    #delete movie by movie ID
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    def delete_movie(id):
+        rc = Movies.delete(id)
+        if rc == 0:
+            return "Movie deletion failed."
+        else:
+            return ("Movie with id=%d deleted successfully" % id)
     
-    @app.route('/actors', methood=['POST'])
+    # Create actor
+    @app.route('/actors', methods=['POST'])
     def create_actor():
-        return "Create actor"
+        actor = Actors(request.get_json().get("name"),request.get_json().get("age"),request.get_json().get("gender"))
+        rc = Actors.insert(actor)
+        if rc == 0:
+            return "Actor creation failed."
+        else:
+            return ("Actor created with id=%d" % rc)
     
-    @app.route('/movies', methood=['POST'])
+    # Create movie
+    @app.route('/movies', methods=['POST'])
     def create_movie():
-        return "Create movie"
+        movie = Movies(request.get_json().get("title"),request.get_json().get("release_date"))
+        rc = Movies.insert(movie)
+        if rc == 0:
+            return "Movie creation failed."
+        else:
+            return ("Movie created with id=%d" % rc )
     
-    @app.route('/actors', methood=['PATCH'])
-    def update_actor():
-        return "Update actor"
+    # Update actor
+    @app.route('/actors/<int:id>', methods=['PATCH'])
+    def update_actor(id):
+        actor = Actors.display(id)
+        actor = json.loads(actor)
+        
+        if id is None or id == 0:
+            return "Actor not found."
+        else:
+            if request.get_json().get("name") is not None:
+                name = request.get_json().get("name")
+            else:
+                name = actor["name"]
+            if request.get_json().get("age") is not None:
+                age = request.get_json().get("age")
+            else:
+                age = actor["age"]
+            if request.get_json().get("gender") is not None:
+                gender = request.get_json().get("gender")
+            else:
+                gender = actor["gender"]
+
+            rc = Actors.update(id,name,age,gender)
+
+        if rc == 0:
+                return "Actor update failed."
+        else:
+                return ("Actor details with id=%d updated successfully" % id)        
     
-    @app.route('/movies', methood=['PATCH'])
-    def update_movie():
-        return "Update movie"
+    @app.route('/movies/<int:id>', methods=['PATCH'])
+    def update_movie(id):
+        movie = Movies.display(id)
+        movie = json.loads(movie)
+        
+        if id is None or id == 0:
+            return "Movie not found."
+        else:
+            if request.get_json().get("title") is not None:
+                title = request.get_json().get("title")
+            else:
+                title = movie["name"]
+            if request.get_json().get("release_date") is not None:
+                releaseDate = request.get_json().get("release_date")
+            else:
+                releaseDate = movie["releaseDate"]
+
+            rc = Movies.update(id,title,releaseDate)
+
+        if rc == 0:
+                return "Movie update failed."
+        else:
+                return ("Movie details with id=%d updated successfully" % id)
 
     return app
 
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from sqlalchemy import Column, String, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
@@ -13,12 +14,13 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
+    # db.create_all()
 
 
 '''
@@ -44,7 +46,8 @@ class Person(db.Model):
   
 class Movies(db.Model):
   __tablename__ = 'Movies'
-  title = Column(String, primary_key=True)
+  id = Column(db.Integer, primary_key=True)
+  title = Column(String)
   release_date = Column(String)
 
   def __init__(self, title, release_date):
@@ -53,8 +56,65 @@ class Movies(db.Model):
 
   def format(self):
     return {
+      'id': self.id,
       'title': self.title,
       'release_date': self.release_date}
+  
+  def __repr__(self):
+    return json.dumps(self.format())
+  
+  #display movie details by id
+  def display(id):
+    movies = Movies.query.filter(Movies.id == id).one_or_none()
+    return format(movies)
+  
+  # display all movies
+  def display_all():
+    movies = Movies.query.all()
+    return format(movies)
+    
+  #insert movie details
+  def insert(self):
+    rc = 0
+    try:
+      db.session.add(self)
+      db.session.commit()
+      rc = self.id
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
+    
+# update Movies details, All the details are mandatory
+  def update(id,title,release_date):
+    rc = 0
+    try:
+      rc = db.session.query(Movies).filter(Movies.id == id).update({Movies.title: title, Movies.release_date: release_date})
+      db.session.commit()
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
+  
+# delete movie details
+  def delete(id):
+    rc = 0
+    try:
+      rc = db.session.query(Movies).filter(Movies.id == id).delete()
+      db.session.commit()
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
   
 class Actors(db.Model):
   __tablename__ = 'Actors'
@@ -74,3 +134,62 @@ class Actors(db.Model):
       'name': self.name,
       'age': self.age,
       'gender': self.gender}
+  
+  def __repr__(self):
+    return json.dumps(self.format())
+  
+  # display actor details by id
+  def display(id):
+    actors = Actors.query.filter(Actors.id == id).one_or_none()
+    if actors is None:
+      return None
+    else:
+      return format(actors)
+  
+  # display all actors
+  def display_all():
+    actors = Actors.query.all()
+    return format(actors)
+
+  # insert Actor details
+  def insert(self):
+    rc = 0
+    try:
+      db.session.add(self)
+      db.session.commit()
+      rc = self.id
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
+
+  # update Actor details, All the details are mandatory
+  def update(id,name,age,gender):
+    rc = 0
+    try:
+      rc = db.session.query(Actors).filter(Actors.id == id).update({Actors.name: name, Actors.age: age,Actors.gender:gender})
+      db.session.commit()
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
+    
+  # delete Actor details
+  def delete(id):
+    rc = 0
+    try:
+      rc = db.session.query(Actors).filter(Actors.id == id).delete()
+      db.session.commit()
+    except:
+      db.session.rollback()
+      print(sys.exc_info())
+      db.session.flush()
+    finally:
+      db.session.close()
+    return rc
